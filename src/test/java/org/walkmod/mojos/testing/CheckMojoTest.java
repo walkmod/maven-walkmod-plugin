07ed4ec6665865caf4a7e5dbe8c3d1b01ae227e1
rpau <rpau@ac.upc.edu>
2015-09-16 21:50:09 +0300

    travis support + unit test + maven 3 support

---
+++ src/test/java/org/walkmod/mojos/testing/CheckMojoTest.java
@@ -0,0 +1,46 @@
+package org.walkmod.mojos.testing;
+
+import java.io.File;
+
+import junit.framework.Assert;
+
+import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+import org.walkmod.mojos.CheckMojo;
+
+
+public class CheckMojoTest extends AbstractMojoTestCase{
+
+	/** {@inheritDoc} */
+    protected void setUp()
+        throws Exception
+    {
+        // required
+        super.setUp();
+    }
+
+    /** {@inheritDoc} */
+    protected void tearDown()
+        throws Exception
+    {
+        // required
+        super.tearDown();
+
+    }
+
+    /**
+     * @throws Exception if any
+     */
+    public void testExecution()
+        throws Exception
+    {
+        File pom = getTestFile( "src/test/resources/project-to-test/pom.xml" );
+        assertNotNull( pom );
+        assertTrue( pom.exists() );
+
+		CheckMojo myMojo = (CheckMojo) lookupMojo("check", pom );
+        assertNotNull( myMojo );
+        myMojo.execute();
+        Assert.assertTrue(true);
+
+    }
+}
